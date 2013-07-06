29cdd47a31635a862f9fcc85e71ee4532e566a2a
Ben Cohen <bn.chn2@gmail.com>
2013-07-05 22:53:56 -0400

    woo!  Stuff!  We have log in/out

---
+++ Thursday-App.py
@@ -0,0 +1,53 @@
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+	 abort, render_template, flash
+
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+
+@app.route('/')
+def index():
+	if not session.get('logged_in'):
+		return render_template("home.html")
+	else:
+		return render_template("user_homepage.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return render_template('user_homepage.html')
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return render_template('home.html')
+
+if __name__ == '__main__':
+	app.run()
+
+
