48d726da607bdf38c0deb8cbc0790f353d5e19e5
Lee Avital <leeavital@gmail.com>
2013-07-14 17:47:15 -0700

    Fixed syntax errors in schema.sql
    Using quotes instead of backticks
    Missing comma in create table statments`

--- schema.sql
+++ schema.sql
@@ -1,17 +1,19 @@
 drop table if exists events;
-CREATE TABLE "events" (
-	"id" INTEGER PRIMARY KEY  NOT NULL ,
-	 "event" VARCHAR NOT NULL ,
-	 "day" INTEGER NOT NULL ,
-	 "month" INTEGER NOT NULL ,
-	 "year" INTEGER NOT NULL
-	 "big" BOOLEAN);
-drop table if exists karma;
-CREATE TABLE "users" (
-	"id" INTEGER PRIMARY KEY  NOT NULL , 
-	"username" VARCHAR NOT NULL  UNIQUE , 
-	"password" VARCHAR NOT NULL , 
-	"admin" BOOL NOT NULL  DEFAULT False);
+CREATE TABLE `events` (
+	`id` INTEGER PRIMARY KEY  NOT NULL ,
+	 `event` VARCHAR NOT NULL ,
+	 `day` INTEGER NOT NULL ,
+	 `month` INTEGER NOT NULL ,
+	 `year` INTEGER NOT NULL,
+	 `big` BOOLEAN);
+
+
+drop table if exists users;
+CREATE TABLE `users` (
+	`id` INTEGER PRIMARY KEY  NOT NULL , 
+	`username` VARCHAR NOT NULL  UNIQUE , 
+	`password` VARCHAR NOT NULL , 
+	`admin` BOOL NOT NULL  DEFAULT False);
 
 
 DROP TABLE IF EXISTS ShoppingList;
